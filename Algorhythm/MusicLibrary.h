b266502ccac92ad60e2c0ddb11a8c52839549131
Jeff Ripke <jripke74@gmail.com>
2017-07-29 17:33:09 -0700

    hard coded music info

---
+++ Algorhythm/MusicLibrary.h
@@ -0,0 +1,15 @@
+//
+//  MusicLibrary.h
+//  Algorhythm
+//
+//  Created by Jeff Ripke on 7/29/17.
+//  Copyright © 2017 Jeff Ripke. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MusicLibrary : NSObject
+
+@property (strong, nonatomic) NSArray *library;
+
+@end
